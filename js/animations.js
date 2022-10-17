096c3a76cbd48b88ca087f773c6c592e76f6b4fb
Artem Sadreev <a.sadreew@yandex.ru>
2022-10-17 19:51:45 +0300

    addOtherInput

--- js/animations.js
+++ js/animations.js
@@ -21,3 +21,28 @@ function increaseNumberAnimationStep (i, element, endNumber) {
     increaseNumberAnimationStep(0, element, 5000)
   }
   initIncreaseNumberAnimation();
+
+  document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+      const formContainer = document.createElement('div')
+      formContainer.classList.add('form__group');
+      formContainer.classList.add('form__other-input');
+
+      const input = document.createElement('input');
+      input.placeholder='Введите ваш вариант';
+      input.type='text';
+      
+      formContainer.appendChild(input);
+      document.querySelector('.form__containter form').insertBefore(formContainer, document.querySelector('.form__submit')); 
+      
+    }
+    const otherInput =  document.querySelector('.form__other-input');
+    if (event.target.value !== 'other'&& otherInput) {
+      // Удаляем ранее добавленное текстовое поле, если оно есть в DOM
+      
+      document.querySelector('.form__containter form').removeChild(otherInput);
+    }
+  });
+
+ 
+ 
