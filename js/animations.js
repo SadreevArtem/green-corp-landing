c660b5dfbadb916ccd9d8f5aaf48b3ec904cfded
Artem Sadreev <a.sadreew@yandex.ru>
2022-10-23 18:52:48 +0300

    addScrollFunctions

--- js/animations.js
+++ js/animations.js
@@ -65,3 +65,27 @@ function increaseNumberAnimationStep (i, element, endNumber) {
 
  window.addEventListener('scroll', updateScroll)
  
+
+ function addSmoothScroll(anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+ 
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+}
+ 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  addSmoothScroll(anchor);
+});
+
+function onLinkClick(event) {
+  event.preventDefault();
+ 
+  document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+addSmoothScroll(document.querySelector('.more-button'));
